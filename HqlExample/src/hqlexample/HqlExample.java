04aadf3058f769fb767c8bf80840afd6a494d170
khalid7487 <khalid15-7487@diu.edu.bd>
2019-11-27 11:45:33 +0600

    error fixed

---
+++ HqlExample/src/hqlexample/HqlExample.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hqlexample;
+
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+/**
+ *
+ * @author KHALID
+ */
+public class HqlExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SessionFactory sessionFactory=HibernateUtil.getSessionFactory();
+        Session session=sessionFactory.openSession();
+        session.beginTransaction();
+        Query query=session.createQuery("from Employee");
+        List list=query.list();
+        session.getTransaction().commit();
+        session.close();
+        System.out.println(list.size());
+    }
+    
+}
